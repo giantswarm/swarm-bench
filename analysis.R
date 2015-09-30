a139501ef9b651d78a67a4524dd07867b8491739
marians <marian@giantswarm.io>
2015-09-30 09:53:59 +0200

    Comments added

--- analysis.R
+++ analysis.R
@@ -3,6 +3,7 @@ library(ggplot2)
 
 data <- read.csv("./data.csv")
 
+# Duration of API calls - Linear Y scale
 ggplot(data, aes(factor(label), duration)) + geom_boxplot() + 
   labs(title="Duration of API calls") +
   xlab("API Call") +
@@ -15,12 +16,14 @@ ggplot(data, aes(factor(label), duration)) + geom_boxplot() +
 #  ylab("Duration (Seconds)") +
 #  scale_y_log10()
 
+# ListServices - duration by route
 listServicesData <- data[data$label == "ListServices", ]
 ggplot(listServicesData, aes(factor(route), duration)) + geom_boxplot() + 
   labs(title="Duration of ListServices calls by route") +
   xlab("Route called") +
   ylab("Duration (Seconds)")
 
+# ServiceStatus - duration by route
 servicesStatusData <- data[data$label == "ServiceStatus", ]
 ggplot(servicesStatusData, aes(factor(route), duration)) + geom_boxplot() + 
   labs(title="Duration of ServiceStatus calls by route") +
